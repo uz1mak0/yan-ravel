fb2d4c9cb82263ef4641a5e34fc4f9d079144a7d
uz1mak0 <uzimako26@gmai.com>
2026-08-31 17:20:48 +0800

    configured to deploy to github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Add this line
+  images: {
+    unoptimized: true, // Required for static export if using next/image
+  },
 };
 
 export default nextConfig;
