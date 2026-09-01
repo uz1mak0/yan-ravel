abe299801cafc80bca00c608265b6034521d8f12
uz1mak0 <uzimako26@gmai.com>
2026-09-01 12:04:45 +0800

    configured next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: 'export', // Add this line
+  output: "export",
+  basePath: isProd ? "/yan-ravel" : "",
   images: {
-    unoptimized: true, // Required for static export if using next/image
+    unoptimized: true,
   },
 };
 
